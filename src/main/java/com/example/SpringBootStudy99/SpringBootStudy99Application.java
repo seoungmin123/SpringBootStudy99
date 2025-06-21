8e18e1130ad3b700e87c96b03cd3c4094b5b439f
이승민 <iseungmin@iseungmin-ui-MacBookPro.local>
2025-06-21 20:11:29 +0900

    Inital commit smZzang

---
+++ src/main/java/com/example/SpringBootStudy99/SpringBootStudy99Application.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootStudy99;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStudy99Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStudy99Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringBootStudy99/SpringBootStudy99ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootStudy99;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStudy99ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
