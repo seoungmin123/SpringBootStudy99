21c2db9433d1c5701b99eb5bdb55acc7e49ff8a4
smOffice <sm1111176@gmail.com>
2025-07-04 17:08:03 +0900

    시큐리티컨피그 수정

--- src/main/java/com/example/SpringBootStudy99/common/SecurityConfig.java
+++ src/main/java/com/example/SpringBootStudy99/common/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable()) //CSRF 사용 안 함
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) //JWT 기반이므로 세션 비활성화 &
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**").permitAll() //api/auth/** (ex. 로그인, 회원가입 등)는 인증 없이 허용
+                        .requestMatchers("/api/user/**").permitAll() //api/auth/** (ex. 로그인, 회원가입 등)는 인증 없이 허용
                         .requestMatchers("/admin/**").hasRole("ADMIN") //hasRole("ADMIN") → ROLE_ADMIN을 의미함 (ROLE_ 접두어 자동 처리)
                         .anyRequest().authenticated()// 그 외 모든 요청은 인증된 사용자만 접근 가능
                 )
